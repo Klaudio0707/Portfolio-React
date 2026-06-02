9d87f3999fc4a4c8a9e372b1577e51ad1d63a2fb
klaudio0707 <claudioroberto0707@outlook.com>
2026-06-01 21:58:22 -0300

    init: React SPA with TypeScript, i18n configuration, and global SCSS architecture

---
+++ portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+
+
+const Header = () => {
+  return (
+    <div className="container_header">
+      <section className="header-right">
+        <h3> Cláudio Roberto</h3>    
+      </section>
+      <section className="header-left">
+            <nav>
+             <ul>
+              <li><a className="nav-link" href="#home">Home</a></li>
+              <li><a className="nav-link" href="#about">About</a></li>
+             </ul>
+            </nav>
+      </section>
+    </div>
+  )
+}
+
+export default Header
