de998bbb34adffa4013501b5183430c71f8cd5bb
klaudio0707 <claudioroberto0707@outlook.com>
2026-06-03 17:56:24 -0300

    style(header): fix mobile navigation alignment by overriding default nav wrapper width

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,26 @@
 import styles from './Header.module.scss';
 import fotoPerfil from '../../assets/foto-perfil.jpeg';
-const Header = () => {
+
+export const Header = () => {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerBrand}>
-        <div className={styles.logoGroup}>
+        <a href="#home" className={styles.logoGroup}>
           <img src={fotoPerfil} alt="Cláudio Roberto" className={styles.microAvatar} />
           <h1 className={styles.logo}>Cláudio Roberto</h1>
-        </div>
+        </a>
       </div>
+      
       <div className={styles.headerNavigation}>
         <nav>
           <ul>
-            <li><a className={styles.navLink} href="#home">Home</a></li>
-            <li><a className={styles.navLink} href="#about">About</a></li>
-            <li><a className={styles.navLink} href="#contact">Contact</a></li>
-            <li><a className={styles.navLink} href="#projects">Projects</a></li>
+            <li><a className={styles.navLink} href="/home">Home</a></li>
+            <li><a className={styles.navLink} href="/about">About</a></li>
+            <li><a className={styles.navLink} href="/projects">Projects</a></li>
+            <li><a className={styles.navLink} href="/contact">Contact</a></li>
           </ul>
         </nav>
       </div>
     </header>
-  )
-}
-
-export default Header
+  );
+};
